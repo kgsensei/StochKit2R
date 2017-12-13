fe2af0584f59e1386cf2d5f808db78097e0b68b1
kevinsanft <kevinsanft@users.noreply.github.com>
2017-12-13 12:51:44 -0500

    documentation updates

--- inst/doc/StochKit2R-UserGuide.R
+++ inst/doc/StochKit2R-UserGuide.R
@@ -1,45 +1,55 @@
-## ----, eval=FALSE--------------------------------------------------------
+## ---- eval=FALSE---------------------------------------------------------
 #  citation(package="StochKit2R")
 
-## ----, eval=FALSE--------------------------------------------------------
+## ---- eval=FALSE---------------------------------------------------------
 #  install.packages("StochKit2R")
 
-## ----, eval=FALSE--------------------------------------------------------
+## ---- eval=FALSE---------------------------------------------------------
+#  install.packages(file.choose(),repos=NULL)
+
+## ---- eval=FALSE---------------------------------------------------------
 #  library(StochKit2R)
 
-## ----, eval=FALSE--------------------------------------------------------
+## ---- eval=FALSE---------------------------------------------------------
+#  install.packages("devtools") #run once on your system
+#  devtools::install_github("kevinsanft/StochKit2R")
+
+## ---- eval=FALSE---------------------------------------------------------
+#  library(StochKit2R)
+
+## ---- eval=FALSE---------------------------------------------------------
 #  install.packages("<path to>/StochKit2R_<version>.tar.gz",repos=NULL,type="source")
 
 ## ----eval=FALSE----------------------------------------------------------
 #  # use the dimer_decay.xml model that is included with StochKit2R
 #  # to make things easier to read, store the path in a variable
 #  model <- system.file("dimer_decay.xml",package="StochKit2R")
-#  # now, run a simulation, you can save the output locally and save it to a selected output:
-#  out <- ssa(model,"ex_out",10,100,20)
+#  # now, run a simulation:
+#  out <- ssa(modelFile=model,time=10,realizations=100,intervals=20)
 
-## ----, eval=FALSE--------------------------------------------------------
-#  out <- ssa("~/Desktop/dimer_decay.xml","~/Desktop/dimer_decay_output",10,100,20)
+## ---- eval=FALSE---------------------------------------------------------
+#  out <- ssa("~/Desktop/dimer_decay.xml",10,100,20)
 
-## ----, eval=FALSE--------------------------------------------------------
-#  out <- ssa("~/Desktop/dimer_decay.xml","~/Desktop/dimer_decay_output",10,100,20,force=T)
+## ---- eval=FALSE---------------------------------------------------------
+#  out <- ssa("~/Desktop/dimer_decay.xml",10,100,20,outputDir="~/Desktop/dimer_decay_output",force=TRUE)
 
-## ----, eval=FALSE--------------------------------------------------------
-#  out <- ssaSingle(system.file("dimer_decay.xml",package="StochKit2R"),"single_output.txt",0,10)
+## ---- eval=FALSE---------------------------------------------------------
+#  out <- ssaSingle(system.file("dimer_decay.xml",package="StochKit2R"),0,10,"single_output.txt")
 
-## ----, fig.width=6, fig.height=4, fig.align="center"---------------------
+## ---- fig.width=6, fig.height=4, fig.align="center"----------------------
 library(StochKit2R)
 #example using included dimer_decay.xml file
 model <- system.file("dimer_decay.xml",package="StochKit2R")
-#output list saved to out
-out <- ssa(model,"ex_out",10,100,20,force=TRUE)
+##output value stored locally for use and written to ex_out file in working directory
+out <- ssa(modelFile=model,time=10,realizations=100,interval=20, outputDir="ex_out",force=TRUE)
 #plot the data for species 2 and 3 (all of them in the dimer decay model)
-plotStats(out,c(2,3))
+plotStats(out$stats,c(2,3))
 
-## ----, eval=FALSE--------------------------------------------------------
+## ---- eval=FALSE---------------------------------------------------------
 #  model <- system.file("dimer_decay.xml",package="StochKit2R")
 #  file.copy(model,"~/Desktop/dimer_decay.xml")
 
-## ----, comment=""--------------------------------------------------------
+## ---- comment=""---------------------------------------------------------
 model <- system.file("dimer_decay.xml",package="StochKit2R")
 XML::xmlParse(model)
 
